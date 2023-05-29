0838245e0b0f393c2da77ae78835f74fc8d0d521
Michael A <michaelabbaspour@gmail.com>
2023-05-28 23:45:31 -0400

    Updated the ContactForm component to properly send a users form submission to my email.

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -2,30 +2,55 @@
  * Contact Form component.
  */
 
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import styles from "../../assets/styles/Contact.module.css";
 
+const emailSubmitParams = {
+	service_id: process.env.EMAIL_SERVICE_ID as string,
+	template_id: process.env.EMAIL_CONTACT_TEMPLATE_ID as string,
+	public_key: process.env.EMAIL_PUBLIC_KEY
+}
+
 export default function ContactForm() {
+	const form = useRef(null);
+	const [formStatus, setFormStatus] = useState<string | null>(null);
+
+	const sendEmail = (e: any) => {
+		e.preventDefault();
+		setFormStatus("submitting");
+
+		emailjs.sendForm(emailSubmitParams.service_id, emailSubmitParams.template_id, form.current!, emailSubmitParams.public_key)
+			.then((result) => {
+				console.log(result.text);
+				setFormStatus("success")
+				e.target.reset();
+			}, (error) => {
+				console.log(error.text);
+				setFormStatus("error")
+			});
+	};
 	return (
 		<div className={styles.contactFormWrapper}>
-			<form className={styles.contactForm}>
+			<form ref={form} onSubmit={sendEmail} className={styles.contactForm}>
 				<div>
 					<label>
-						First Name <input type="text" name="firstName" required />
+						First Name <input type="text" name="first_name" required />
 					</label>
 				</div>
 				<div>
 					<label>
-						Last Name <input type="text" name="lastName" required />
+						Last Name <input type="text" name="last_name" required />
 					</label>
 				</div>
 				<div>
 					<label>
-						Company Name <input type="text" name="companyName" />
+						Company Name <input type="text" name="company_name" />
 					</label>
 				</div>
 				<div>
 					<label>
-						Email <input type="email" name="email" required />
+						Email <input type="email" name="user_email" required />
 					</label>
 				</div>
 				<div>
@@ -40,6 +65,20 @@ export default function ContactForm() {
 				</div>
 				<div className={styles.contactFormButtonWrapper}>
 					<button type="submit" className={styles.contactFormButton}>Send</button>
+				</div>
+				<div className={styles.contactFormSubmitStatus}>
+					{formStatus === "submitting" && (
+						<div className={styles.contactFormStatusSubmitting}>Submitting...</div>
+					)
+					}
+					{formStatus === "success" && (
+						<div className={styles.contactFormStatusSuccess}>Success! Message sent.</div>
+					)
+					}
+					{formStatus === "error" && (
+						<div className={styles.contactFormStatusError}>Message failed to send.</div>
+					)
+					}
 				</div>
 			</form>
 		</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const defines = Object.entries(env).reduce((prev, [key, value]) => {
+    return {
+      ...prev,
+      [`process.env.${key}`]: `"${value}"`,
+    };
+  }, {});
+
+  return {
+    plugins: [react()],
+    define: defines,
+  };
+});
