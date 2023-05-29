5f67876be51e476d00550facb4d9f30f163c405f
Michael A <michaelabbaspour@gmail.com>
2023-05-29 19:29:02 -0400

    Updated ContactForm.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd());
   const defines = Object.entries(env).reduce((prev, [key, value]) => {
     return {
       ...prev,
